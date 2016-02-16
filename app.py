9e771a6f0d6973b77ae4ab57a1778033c2a0d1a7
ylambda <ylambda@koalabeast.com>
2016-02-16 15:16:36 -0500

    Update database

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify, url_for, abort
 
 app = Flask(__name__)
 
-quotes_db = []
-with open("quotes_db.json") as db:
-    quotes_db = json.load(db)
+database = {}
+with open("database.json") as db:
+    database = json.load(db)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -22,22 +22,22 @@ def quote_random(quotes):
     return random.choice(quotes)
 
 def quotes_by_tag(tag):
-    return [quote for quote in quotes_db if tag in quote["tags"]]
+    return [quote for quote in database.get("quotes") if tag in quote["tags"]]
 
 @app.route("/v0/quotes", methods=["GET"])
 def get_quotes():
-    return jsonify({"quotes": [make_public_quote(quote) for quote in quotes_db]})
+    return jsonify({"quotes": [make_public_quote(quote) for quote in database.get("quotes")]})
 
 @app.route("/v0/quotes/<int:quote_id>", methods=["GET"])
 def get_quote(quote_id):
-    quote = [quote for quote in quotes_db if quote["id"] == quote_id]
+    quote = [quote for quote in database.get("quotes") if quote["id"] == quote_id]
     if len(quote) == 0:
         abort(404)
     return jsonify(quote[0])
 
 @app.route("/v0/quotes/random", methods=["GET"])
 def get_quote_random():
-    return jsonify(make_public_quote(quote_random(quotes_db)))
+    return jsonify(make_public_quote(quote_random(database.get("quotes"))))
 
 @app.route("/v0/quotes/tag/<string:tag>", methods=["GET"])
 def get_quotes_by_tag(tag):
