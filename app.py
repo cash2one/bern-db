bcb8c20c1671108eb5a9784b00b9c65624581e5e
ylambda <ylambda@koalabeast.com>
2016-02-21 19:50:08 -0500

    Add rate limiting and flask security

--- app.py
+++ app.py
@@ -2,22 +2,53 @@ import random
 import config
 
 from flask import Flask, jsonify, url_for, abort
-from flask_sqlalchemy import SQLAlchemy
-
-import flask_admin as admin
+from flask_admin import Admin
 from flask_admin.contrib import sqla
+from flask_sqlalchemy import SQLAlchemy
+from flask_limiter import Limiter
+from flask_security import Security, SQLAlchemyUserDatastore, login_required, current_user
 
 app = Flask(__name__)
 app.config.from_object("config")
+
 db = SQLAlchemy(app)
+limiter = Limiter(app)
 
 import models
 
-class TagAdmin(sqla.ModelView):
+
+user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
+security = Security(app, user_datastore)
+
+
+class MyModelView(sqla.ModelView):
+
+    def is_accessible(self):
+        if not current_user.is_active or not current_user.is_authenticated:
+            return False
+
+        if current_user.has_role('superuser'):
+            return True
+
+        return False
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
+
+class TagAdmin(MyModelView):
     def __init__(self, session):
         super(TagAdmin, self).__init__(models.Tag, session)
 
-class QuoteAdmin(sqla.ModelView):
+class QuoteAdmin(MyModelView):
     form_ajax_refs = {
         "tags": {
             "fields": (models.Tag.name,)
@@ -25,14 +56,25 @@ class QuoteAdmin(sqla.ModelView):
     }
     def __init__(self, session):
         super(QuoteAdmin, self).__init__(models.Quote, session)
-
-admin = admin.Admin(app, name="ASDASD")
+admin = Admin(app, name="ASDASD")
 admin.add_view(QuoteAdmin(db.session))
 admin.add_view(TagAdmin(db.session))
+admin.add_view(MyModelView(models.Role, db.session))
+admin.add_view(MyModelView(models.User, db.session))
+
+for view in admin._views:
+    limiter.exempt(view.blueprint)
 
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "Not Found"}), 404
+
+@app.errorhandler(429)
+def not_found(error):
+    return jsonify({
+        "error": "Too Many Requests",
+        "description": error.description
+    }), 429
 
 def quotes_by_tag(tag):
     return [quote for quote in database.get("quotes") if tag in quote["tags"]]
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin
 
 from app import db
 from schema import TagSchema, QuoteSchema
@@ -36,3 +37,33 @@ class Quote(db.Model):
 
     def __str__(self):
         return self.text
+
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __str__(self):
+        return self.email
