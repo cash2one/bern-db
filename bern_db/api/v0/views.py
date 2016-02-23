37aac32cfc99292b2ff13886c6c350fb4bc11981
ylambda <ylambda@koalabeast.com>
2016-02-23 12:31:46 -0500

    Quick fix to allow CORS

--- bern_db/api/v0/views.py
+++ bern_db/api/v0/views.py
@@ -2,6 +2,12 @@ from flask import Blueprint, jsonify
 from ..models import Quote, Tag
 
 v0 = Blueprint("v0", __name__, url_prefix="/v0")
+headers = {
+    "Access-Control-Allow-Origin": "*"
+}
+
+def make_response(body):
+    return (body, 200, headers)
 
 @v0.route("/quotes", methods=["GET"])
 def get_quotes():
@@ -10,7 +16,7 @@ def get_quotes():
     for quote in quotes:
         public_quotes.append(quote.make_public())
 
-    return jsonify({"quotes": public_quotes})
+    return make_response(jsonify({"quotes": public_quotes}))
 
 @v0.route("/quotes/<int:quote_id>", methods=["GET"])
 def get_quote(quote_id):
@@ -20,20 +26,22 @@ def get_quote(quote_id):
 @v0.route("/quotes/random", methods=["GET"])
 def get_quote_random():
     public_quote = Quote.get_random().make_public()
-    return jsonify(public_quote)
+    return make_response(jsonify(public_quote))
 
 @v0.route("/quotes/tags", methods=["GET"])
 def get_quote_tags():
     tags = Tag.query.all()
     public_tags = [tag.name for tag in tags]
-    return jsonify({"tags": public_tags})
+    return make_response(jsonify({"tags": public_tags}))
 
 @v0.route("/quotes/tags/<string:tag_name>", methods=["GET"])
 def get_quotes_by_tag(tag_name):
     public_quotes = Tag.get_by_name(tag_name).make_public_quotes()
-    return jsonify({"quotes": public_quotes})
+    return make_response(jsonify({"quotes": public_quotes}))
 
 @v0.route("/quotes/tags/<string:tag_name>/random", methods=["GET"])
 def get_quote_by_tag_random(tag_name):
     public_quote = Quote.get_by_random_tag_name(tag_name).make_public()
-    return jsonify(public_quote)
+    return make_response(jsonify(public_quote))
+
+
